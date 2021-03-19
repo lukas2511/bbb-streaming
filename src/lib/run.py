49e8e296e66e7b46cf122dc3429851c2dfafea4d
Lukas Schauer <lukas@schauer.dev>
2021-03-19 19:58:43 +0100

    only allow commands from moderators, also allow commands in private chats

--- src/lib/run.py
+++ src/lib/run.py
@@ -51,16 +51,48 @@ def start(join_url, rtmp_url, background):
     presentationstream = presentation.Presentation(sessionmanager, screenshareswitch)
     screensharemanager = screenshare.ScreenshareManager(sessionmanager, screenshareswitch)
 
-    def chatmsg(msg):
-        if 'collection' not in msg or msg['collection'] != 'group-chat-msg':
-            return
-        print(msg['fields']['sender'] + ": " + msg['fields']['message'])
+    def sendmsg(txt, chatid='MAIN-PUBLIC-GROUP-CHAT'):
+        timestamp = int(time.time())
+        msg = {}
+        msg['msg'] = 'method'
+        msg['method'] = 'sendGroupChatMsg'
+        msg['params'] = []
+        msg['params'].append(chatid)
+        msg['params'].append({'color': '0', 'correlationId': '%s-%d' % (sessionmanager.bbb_info['internalUserID'], timestamp), 'sender': {'id': sessionmanager.bbb_info['internalUserID'], 'name': sessionmanager.bbb_info['fullname']}, 'message': txt})
+        msg['id'] = 'fnord-chat-%d' % timestamp
+        sessionmanager.send(msg)
 
-        txt = msg['fields']['message']
-        if txt.startswith("!") and ' ' in txt:
-            cmd, args = txt[1:].split(' ', 1)
-            if cmd == 'view':
-                streammixer.set_view(args)
+    def chatmsg(msg):
+        if 'collection' not in msg:
+            return
+
+        if msg['collection'] == 'group-chat':
+            rply = {}
+            rply['msg'] = 'sub'
+            rply['name'] = 'group-chat-msg'
+            rply['params'] = [[msg['fields']['chatId']]]
+            rply['id'] = 'fnord-group-' + msg['fields']['chatId']
+            sessionmanager.send(rply)
+
+        elif msg['collection'] == 'group-chat-msg':
+            if msg['fields']['sender'] == sessionmanager.bbb_info['internalUserID']:
+                return
+
+            txt = msg['fields']['message']
+            sender = sessionmanager.get_user_by_internal_id(msg['fields']['sender'])
+            if not sender:
+                return
+
+            print("%s (%s,%s): %s" % (sender['name'], sender['role'], 'PUBLIC' if msg['fields']['chatId'] == 'MAIN-PUBLIC-GROUP-CHAT' else 'PRIVATE', txt))
+            if sender['role'] != 'MODERATOR':
+                if msg['fields']['chatId'] != 'MAIN-PUBLIC-GROUP-CHAT':
+                    sendmsg('You have no permissions to use this command', msg['fields']['chatId'])
+                return
+
+            if txt.startswith("!") and ' ' in txt:
+                cmd, args = txt[1:].split(' ', 1)
+                if cmd == 'view':
+                    streammixer.set_view(args)
     sessionmanager.attach(chatmsg)
 
     class MyShell(cmd.Cmd):
@@ -71,15 +103,7 @@ def start(join_url, rtmp_url, background):
                 camera.force_keyframe()
 
         def do_say(self, arg):
-            timestamp = int(time.time())
-            msg = {}
-            msg['msg'] = 'method'
-            msg['method'] = 'sendGroupChatMsg'
-            msg['params'] = []
-            msg['params'].append('MAIN-PUBLIC-GROUP-CHAT')
-            msg['params'].append({'color': '0', 'correlationId': '%s-%d' % (sessionmanager.bbb_info['internalUserID'], timestamp), 'sender': {'id': sessionmanager.bbb_info['internalUserID'], 'name': sessionmanager.bbb_info['fullname']}, 'message': arg})
-            msg['id'] = 'fnord-chat-%d' % timestamp
-            sessionmanager.send(msg)
+            sendmsg(arg)
 
         def do_view(self, arg):
             streammixer.set_view(arg)
